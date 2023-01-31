31a29873f8303b86a81677d6ee0fef8493e1f72d
orianl <orian.levy@forescout.com>
2023-01-31 16:36:53 +0200

    refactor

--- src/components/Scores/useScores.tsx
+++ src/components/Scores/useScores.tsx
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from 'react';
+import {useMemo, useState} from 'react';
 import {useQuery} from "react-query";
 
 const useScores = (numberOfRowsInPage: number) => {
